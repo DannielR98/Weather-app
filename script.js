92b0e0eb2ae7f3e876594218772daca7f78c85ed
Daniel_Racho <daniel.racho98@gmail.com>
2023-10-31 09:50:05 +0100

    The initial visibile city wasn't updating...Will look into it

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ const apiKey = "d2b240e1c10f5c7cbea860fa0329d8f8";
 
 
     
-    window.onload = function()
-    {
-        initial();
-    }
+
     
     async function checkWeather(city){
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -59,44 +56,7 @@ const apiKey = "d2b240e1c10f5c7cbea860fa0329d8f8";
 
 
 
-    async function initial()
-    {
-        response = await fetch(response_initial);
 
-        
-         
-            var data = await response.json();
-
-        
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML =Math.round(data.main.temp)  + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-        
-
-        if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png"; 
-        }
-        else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png"; 
-        }
-        else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png"; 
-        }
-        else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png"; 
-        }
-        else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png"; 
-        }
-        else if (data.weather[0].main == "Snow") {
-            weatherIcon.src = "images/snow.png"; 
-        }
-
-        document.querySelector(".weather").style.display = "block";
-        document.querySelector(".error").style.display = "none";
-        
-    }
 
 
     
